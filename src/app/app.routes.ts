52c7ce4235b6c5c89c57f2dff4cc3f36e6f46ece
André Luferat <contato@luferat.net>
2025-09-22 18:46:55 -0300

    Muita coisa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Contacts } from './contacts/contacts';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Rotas vazias redirecionam para a `/home` ← Sempre a primeira
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+    // Páginas { rota, componente, título }
+    { path: 'home', component: Home, title: 'NgCRUD' },
+    { path: 'contacts', component: Contacts, title: 'NgCRUD - Faça Contato' },
+    { path: 'about', component: About, title: 'NgCRUD - Sobre' },
+
+    // Rota coringa para redirecionar caminhos inválidos ← Sempre a última
+    { path: '**', redirectTo: '/home' }
+];
